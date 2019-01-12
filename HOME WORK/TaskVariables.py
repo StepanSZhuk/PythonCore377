46b11dad349584f3e96bb498a6c327be32f84548
StepanSZhuk <46113363+StepanSZhuk@users.noreply.github.com>
2019-01-12 20:34:12 +0200

    Practical TASK for Homework1
    Practical TASK for Homework1

---
+++ HOME WORK/TaskVariables.py
@@ -0,0 +1,11 @@
+var_int=10
+var_float=8.4
+var_str="No"
+big_int=var_int*3.5
+var_float=var_float-1.0
+print(big_int)
+print(var_float)
+print(var_int/var_float)
+print(big_int/var_float)
+var_str="No"*2+"YES"*3
+print(var_str)
