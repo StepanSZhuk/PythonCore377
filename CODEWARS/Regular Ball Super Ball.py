0a58fa4a936c2ee01d0207a319d957bf8c929abd
StepanSZhuk <46113363+StepanSZhuk@users.noreply.github.com>
2019-02-09 15:11:47 +0200

    Create a class Ball

---
+++ CODEWARS/Regular Ball Super Ball.py
@@ -0,0 +1,11 @@
+#Codewars Stepan Zhuk
+#Create a class Ball.
+#Ball objects should accept one argument for "ball type" when instantiated.
+#If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+
+class Ball(object):
+    def __init__ (self, ball_type="regular"):
+        self.ball_type="regular"
+        if ball_type=="super":
+            self.ball_type="super"
